48e8130e0ce831dcaa6393001a58a845582dbd2e
Mark A Guza <guzamaguza@gmail.com>
2020-04-19 23:10:03 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,7 +12,7 @@ class Song < ActiveRecord::Base
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
     if !Artist.name == "Drake"
       drake = Artist.new("Drake")
-      self.artist = drake 
+      self.artist = drake
     end
 
   end
